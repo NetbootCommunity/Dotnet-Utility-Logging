bbbbbadec3f42cee3f66e957703f6310c748975e
Thomas ILLIET <contact@thomas-illiet.fr>
2022-03-20 22:00:21 +0100

    Update LogOption.cs

--- Src/Models/LogOption.cs
+++ Src/Models/LogOption.cs
@@ -1,6 +1,6 @@
 ﻿namespace Netboot.Logging.Models
 {
-    public class LogOption
+    public class LogConfiguration
     {
         public LogFileOption File { get; set; }
         public LogConsoleOption Console { get; set; }
